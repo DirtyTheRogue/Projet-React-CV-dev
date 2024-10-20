4453abbb4c1626aebb23dd2b965c5ebd5415e2de
DirtyTheRogue <112849284+DirtyTheRogue@users.noreply.github.com>
2024-10-20 18:57:28 +0200

    fin

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // URL du backend hébergé sur Render
 const API_URL = 'https://projet-react-cv-dev.onrender.com/api';
 
+
 // Configuration de l'instance axios avec l'URL de base
 const api = axios.create({
   baseURL: API_URL,
