fea7178bf0651e2bdebcee4d68d9cebc79f67f42
DirtyTheRogue <112849284+DirtyTheRogue@users.noreply.github.com>
2024-10-20 02:50:02 +0200

    index pb

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-
-=======
->>>>>>> 0053653b38a6283d146602f9bba7c0a2c2ab3041
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,6 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
